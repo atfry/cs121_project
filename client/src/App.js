275c15e9242164e3e7e45dc3d4531fbdea7b82d7
Riti Sharma <riti.sharma11@yahoo.com>
2019-09-29 07:47:46 -0700

    rs registor form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,48 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Nav from './components/Nav';
+import RegisterForm from './components/RegisterForm';
 import AllRides from './components/AllRides';
 import RideRequest from './components/RideRequest';
 import { Route, withRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+      registerFormData: {
+        username: '',
+        password: '',
+        email: ''
+      }
+
+    }
+  }
+
+  handleRegisterFormChange = (ev) => {
+    const { name, value } = ev.target;
+
+    this.setState(prevState => ({
+      registerFormData: {
+        ...prevState.registerFormData,
+        [name]: value
+      }
+    }));
+  }
+
+  handleRegisterSubmit = async (ev) => {
+    ev.preventDefault();
+    this.setState({
+      registerFormData: {
+        username: '',
+        password: '',
+        email: ''
+      }
+    })
+    console.log(this.registerFormData);
+  }
 
 
 
@@ -17,6 +52,13 @@ class App extends React.Component {
       <div className="App">
         <Nav />
         <Header />
+
+        <RegisterForm
+          registerFormData={this.state.registerFormData}
+          handleRegisterSubmit={this.handleRegisterSubmit}
+          handleRegisterFormChange={this.handleRegisterFormChange}
+        />
+
         <Route path="/home" render={() => (
           <Home />
         )} />
